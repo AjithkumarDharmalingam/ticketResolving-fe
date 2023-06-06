5966448015176bd9d97ed0c502bc68d607ab6260
AjithkumarDharmalingam <ajithkumarbe97@gmail.com>
2023-06-06 16:33:09 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
+
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
+// import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
 import { configureStore } from "@reduxjs/toolkit";
 import {
@@ -14,6 +15,11 @@ import {
 import pagestate from "./components/Reducers/pagestate";
 import user from "./components/Reducers/user";
 import query from "./components/Reducers/query";
+import { persistReducer } from "redux-persist";
+import { combineReducers } from "@reduxjs/toolkit";
+import storage from "redux-persist/lib/storage";
+import { PersistGate } from "redux-persist/integration/react";
+import { persistStore } from "redux-persist";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 let theme = createTheme({
@@ -33,18 +39,31 @@ let theme = createTheme({
   }
 });
 theme = responsiveFontSizes(theme);
-const store = configureStore({
-  reducer: {
-    pagestate: pagestate,
-    user: user,
-    query: query
-  }
+
+const persistConfig = {
+  key: "root",
+  version: 1,
+  storage
+};
+
+const reducer = combineReducers({
+  pagestate: pagestate,
+  user: user,
+  query: query
 });
+
+const persistedReducer = persistReducer(persistConfig, reducer);
+const store = configureStore({
+  reducer: persistedReducer
+});
+let persistor = persistStore(store);
 root.render(
   <ThemeProvider theme={theme}>
     <Provider store={store}>
       <BrowserRouter>
-        <App />
+        <PersistGate persistor={persistor}>
+          <App />
+        </PersistGate>
       </BrowserRouter>
     </Provider>
   </ThemeProvider>
@@ -53,4 +72,4 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
